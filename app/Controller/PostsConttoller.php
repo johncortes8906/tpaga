ce5a81dce71ef7baf41ad2040aa6a3e7c79ccafe
root <root@127.0.0.1localhost>
2019-03-17 00:51:11 -0500

    initialize project

---
+++ app/Controller/PostsConttoller.php
@@ -0,0 +1,11 @@
+<?php
+
+class PostsConttoller extends AppConttoller{
+
+    public $helpers = array('Html', 'Form');
+
+    public function index()
+    {
+        //$ths->set('posts', $this->Post->find('all'));
+    }
+}
